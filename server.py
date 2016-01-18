6fc0e3d9b0867a0abc87cdf0c3cbef2c2854dc63
Jean Amiouny <jean.amiouny@gmail.com>
2016-01-17 19:06:02 -0500

    asdda

--- server.py
+++ server.py
@@ -11,16 +11,39 @@ app = Flask(__name__)
 wallet = Wallet()
 payment = Payment(app, wallet)
 
+
+def bad_request(message):
+    response = jsonify({'message': message})
+    response.status_code = 400
+    return response
+
+
 def get_price(request):
-    return int(request.args['price'])
+    bad_arguments = []
+    if not request.args.get('address'):
+        bad_arguments.append('address')
+
+    if not len(bad_arguments):
+        price =  10# validator
+    else:
+        setattr(request, 'bad_arguments', bad_arguments)
+        price = 0
+
+    return price
+
 
 @app.route('/order')
 @payment.required(get_price)
 def order():
+    bad_arguments = getattr(request, 'bad_arguments')
+    if hasattr(request, 'bad_arguments'):
+        return bad_request('Invalid request. Please check your argument(s): {}'.format(', '.join(bad_arguments)))
+
     return 'Awesome'
 
+
 @app.route('/validate')
-@payment.required(100)
+@payment.required(1)
 def validate():
     return 'nothing'
 
