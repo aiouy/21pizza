3e7a0ddc0b05dc6bfcbb96205b54266313eb7f76
Jean Amiouny <jean.amiouny@gmail.com>
2016-01-18 02:25:59 -0500

    server<->order

--- server.py
+++ server.py
@@ -19,42 +19,22 @@ def bad_request(message):
 
 
 def get_price(request):
-    data_to_send = {
-        "customer": {
-            "firstName": "Alex",
-            "lastName": "Bitcoins",
-            "address": {
-                "Street": "532 Tyrella Ave #39",
-                "City": "Mountain View",
-                "Region": "CA",
-                "PostalCode": "94043"
-            },
-            "phone": "8023564779",
-            "email": "habs7707@gmail.com"
-        },
+    # r = requests.post(url='http://localhost:3000/validateAndPrice', json=request.data)
+    # response_status = json.loads(r.text)["result"]["Status"]
+    #
+    # if int(response_status) == 1 or int(response_status) == 0:
+    #     price_in_usd = json.loads(r.text)["result"]["Order"]["Amounts"]["Payment"]
+    #
+    #     get_bitpay_btc_usd_rate = urllib.request.urlopen(url="https://bitpay.com/api/rates/usd").read().decode("utf-8")
+    #     usd_per_btc = json.loads(get_bitpay_btc_usd_rate)["rate"]
+    #
+    #     # price = int(price_in_usd * 10**8 / usd_per_btc)
+    #     price = 100
+    # else:
+    #     setattr(request, 'error_validate', 'error_validate')
+    #     price = 0
 
-        "item": {
-            "code": "W40PHOTW",
-            "options": [],
-            "quantity": 1
-        },
-
-        "storeID": "7931"
-    }
-    r = requests.post(url='http://localhost:3000/validateAndPrice', json=request.data)
-    response_status = json.loads(r.text)["result"]["Status"]
-
-    if int(response_status) == 1 or int(response_status) == 0:
-        price_in_usd = json.loads(r.text)["result"]["Order"]["Amounts"]["Payment"]
-
-        get_bitpay_btc_usd_rate = urllib.request.urlopen(url="https://bitpay.com/api/rates/usd").read().decode("utf-8")
-        usd_per_btc = json.loads(get_bitpay_btc_usd_rate)["rate"]
-
-        # price = int(price_in_usd * 10**8 / usd_per_btc)
-        price = 100
-    else:
-        setattr(request, 'error_validate', 'error_validate')
-        price = 0
+    price = 100
 
     return price
 
