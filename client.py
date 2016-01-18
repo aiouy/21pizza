f9b0635223309f2f0a3181945aa740b773dc4870
Jean Amiouny <jean.amiouny@gmail.com>
2016-01-17 20:11:58 -0500

    asdda

--- client.py
+++ client.py
@@ -62,7 +62,7 @@ def pizza():
     ########### DO NOT EDIT BELOW ###########
 
     order_url = server_url+'order'
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    headers = {'Content-type': 'application/json'}
     answer = requests.post(url=order_url, json=order_object, headers=headers)
 
     return answer.text
