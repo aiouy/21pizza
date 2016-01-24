a0e69ec1b5a57eefc7be0371d24b8912bfae34c5
Jean Amiouny <jean.amiouny@gmail.com>
2016-01-24 00:28:37 -0500

    add test-script.py

--- tests/test-script.py
+++ tests/test-script.py
@@ -24,9 +24,24 @@ data_to_send = {
 }
 
 # get menu
-find_stores_url = server_url + 'getMenuForStoreID?zipCode=' + data_to_send['customer']['address']['PostalCode']
-r = requests.get(url=find_stores_url).json()
+# find_stores_url = server_url + 'getMenuForStoreID?zipCode=' + data_to_send['customer']['address']['PostalCode']
+# r = requests.get(url=find_stores_url).json()
 
 # validate the user's order and return the price in USD
-resp = requests.post(server_url + 'validate', data=data_to_send)
-print(resp.text)
+resp = json.loads(requests.post(server_url + 'validate', json=data_to_send).text)
+
+# check if order details are valid
+if resp['status'] == 'success':
+    # ask user for confirmation
+    confirm = input(resp['text']+'\n')
+    if confirm == 'yes':
+        print('Placing order...')
+        order = json.loads(requests.post(server_url + 'order', json=data_to_send).text)
+        print(order['text'])
+        # if error, exit
+        if order['status'] == 'error':
+            sys.exit(1)
+
+else:
+    print(resp['text'])
+    sys.exit(1)
